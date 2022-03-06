95fdabd051da385d2803b41394e961d23df6675e
DemoTest543216 <demotest543216@gmail.com>
2022-03-06 10:26:47 +0530

    added New Filed

--- EcomerceCart-service/EcomerceCart-service/src/main/java/com/sanjib/cart/cartModel/CartDetailsModel.java
+++ EcomerceCart-service/EcomerceCart-service/src/main/java/com/sanjib/cart/cartModel/CartDetailsModel.java
@@ -10,15 +10,16 @@ public class CartDetailsModel {
 
 	@NotNull(message = "Please Provide User id")
 	private Integer userId;
-	
+
 	@NotNull(message = "Please Provide product id")
 	private Integer productId;
-	
+
 	@NotBlank(message = "Please Provide product Name")
 	private String productName;
-	
+
 	@NotNull(message = "Please Provide product Quantity")
 	private Integer quntity;
-	
-	
+
+	private String prodSize;
+
 }
